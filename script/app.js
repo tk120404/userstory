6661092a312bb15d9126e1c24d74832594f93629
tk120404 <arjunkumartk@gmail.com>
2014-06-20 00:48:57 +0530

    3 more templates

--- script/app.js
+++ script/app.js
@@ -1,7 +1,18 @@
 (function() {
     var app = angular.module('scrumstory', ['ui.bootstrap', 'ui.router']);
+    app.run(
+        ['$rootScope', '$state',
+            function($rootScope, $state) {
+               $rootScope.isTemplateSelected = function(template)
+                {
+                	return $state.includes(template);
+                		
+                }
+                $rootScope.roles = ['user', 'developer', 'end-user', 'admin', 'teacher', 'engineer', 'artist', 'PO', 'manager', 'doctor', 'vendor', 'scientist'];
+
+            }
+        ])
     app.config(function($stateProvider, $urlRouterProvider) {
-        // For any unmatched url, send to /route1
         $urlRouterProvider.otherwise("/template1")
         $stateProvider.state('template1', {
             url: "/template1",
@@ -11,15 +22,13 @@
                 $scope.blockquoteHide = false;
                 $scope.finalText = "";
                 $scope.finalTextShow = false;
-                $scope.roles = ['user', 'developer', 'end-user', 'admin', 'teacher', 'engineer', 'artist', 'PO', 'manager', 'doctor', 'vendor', 'scientist'];
+               //scope.roles = ['user', 'developer', 'end-user', 'admin', 'teacher', 'engineer', 'artist', 'PO', 'manager', 'doctor', 'vendor', 'scientist'];
                 $scope.storyReady = function() {
-                    console.log($scope.blockquoteHide, $scope.finalTextShow);
                     if ($scope.story.role && $scope.story.goal && $scope.story.benefit) {
                         $scope.finalText = "As a " + $scope.story.role + ", I want " + $scope.story.goal + ", so that " + $scope.story.benefit;
                         $scope.blockquoteHide = true;
                         $scope.finalTextShow = true;
                     }
-                    console.log($scope.blockquoteHide, $scope.finalTextShow);
                 }
                 $scope.reset = function() {
                     $scope.story = {};
@@ -34,6 +43,93 @@
                     $event.target.select();
                 }
             }
+        }).state('template2', {
+            url: "/template2",
+            templateUrl: "views/template2.html",
+            controller: function($scope) {
+                $scope.story = {};
+                $scope.blockquoteHide = false;
+                $scope.finalText = "";
+                $scope.finalTextShow = false;
+              //$scope.roles = ['user', 'developer', 'end-user', 'admin', 'teacher', 'engineer', 'artist', 'PO', 'manager', 'doctor', 'vendor', 'scientist'];
+              	$scope.storyReady = function() {
+                    if ($scope.story.role && $scope.story.goal) {
+                        $scope.finalText = "As a " + $scope.story.role + ", I want " + $scope.story.goal;
+                        $scope.blockquoteHide = true;
+                        $scope.finalTextShow = true;
+                    }
+                }
+                $scope.reset = function() {
+                    $scope.story = {};
+                    $scope.blockquoteHide = false;
+                    $scope.finalText = "";
+                    $scope.finalTextShow = false;
+                }
+                $scope.isDisable = function() {
+                    return !($scope.story.role && $scope.story.goal);
+                }
+                $scope.highlightMe = function($event) {
+                    $event.target.select();
+                }
+            }
+        }).state('template3', {
+            url: "/template3",
+            templateUrl: "views/template3.html",
+            controller: function($scope) {
+                $scope.story = {};
+                $scope.blockquoteHide = false;
+                $scope.finalText = "";
+                $scope.finalTextShow = false;
+              //$scope.roles = ['user', 'developer', 'end-user', 'admin', 'teacher', 'engineer', 'artist', 'PO', 'manager', 'doctor', 'vendor', 'scientist'];
+                $scope.storyReady = function() {
+                    if ($scope.story.role && $scope.story.goal && $scope.story.benefit) {
+                        $scope.finalText = "In order to " + $scope.story.benefit + " as a " + $scope.story.role + ", I want " + $scope.story.goal;
+                        $scope.blockquoteHide = true;
+                        $scope.finalTextShow = true;
+                    }
+                }
+                $scope.reset = function() {
+                    $scope.story = {};
+                    $scope.blockquoteHide = false;
+                    $scope.finalText = "";
+                    $scope.finalTextShow = false;
+                }
+                $scope.isDisable = function() {
+                    return !($scope.story.benefit && $scope.story.role && $scope.story.goal);
+                }
+                $scope.highlightMe = function($event) {
+                    $event.target.select();
+                }
+            }
+        }).state('template4', {
+            url: "/template4",
+            templateUrl: "views/template4.html",
+            controller: function($scope) {
+                $scope.story = {};
+                $scope.blockquoteHide = false;
+                $scope.finalText = "";
+                $scope.finalTextShow = false;
+               //scope.roles = ['user', 'developer', 'end-user', 'admin', 'teacher', 'engineer', 'artist', 'PO', 'manager', 'doctor', 'vendor', 'scientist'];
+                $scope.storyReady = function() {
+                    if ($scope.story.role && $scope.story.when && $scope.story.where && $scope.story.what && $scope.story.why ) {
+                        $scope.finalText = "As " + $scope.story.role +" " +$scope.story.when +" " + $scope.story.where + ", I " + $scope.story.what + " because " + $scope.story.why;
+                        $scope.blockquoteHide = true;
+                        $scope.finalTextShow = true;
+                    }
+                }
+                $scope.reset = function() {
+                    $scope.story = {};
+                    $scope.blockquoteHide = false;
+                    $scope.finalText = "";
+                    $scope.finalTextShow = false;
+                }
+                $scope.isDisable = function() {
+                    return !($scope.story.role && $scope.story.when && $scope.story.where && $scope.story.what && $scope.story.why );
+                }
+                $scope.highlightMe = function($event) {
+                    $event.target.select();
+                }
+            }
         })
     });
     app.directive('autoSelect', function($parse) {
@@ -42,7 +138,7 @@
                 var model = $parse(attrs.autoSelect);
                 scope.$watch(model, function(value) {
                     if (value === true) {
-                    	console.log(value);
+                        console.log(value);
                         element[0].select();
                     }
                 });
